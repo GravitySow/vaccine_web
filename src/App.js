7457772b6cee379b6000298f194ebc0fe470bd0f
meen.ufo <meen.ufo@gmail.com>
2022-04-17 15:47:17 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import LoginPage from './pages/admin/login/LoginPage.js'
+import HomePage from './pages/admin/home/HomePage.js'
+import DashboardPage from './pages/admin/dashboard/DashboardPage'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} > </Route>
+          <Route path="/login" element={<LoginPage />} > </Route>
+          <Route path="/dashboard" element={<DashboardPage />} > </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
